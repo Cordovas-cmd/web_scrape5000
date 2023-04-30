0e09eea62da66a31abc48de91657486767c0ad7c
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-04-30 02:45:59 -0500

    working with data

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 const searchBtn = document.getElementById('searchBtn');
 
+const resultsContainer = document.getElementById("results")
+
 const googleAPIUrl = "https://www.googleapis.com/youtube/v3/search";
 
 searchBtn.addEventListener("click", (e) => {
@@ -20,8 +22,17 @@ searchBtn.addEventListener("click", (e) => {
 
             // for each runs for each item in the results.
             results.items.forEach(item => {
+                // grabbing the video id but would need to format it into a link
+                if (item.id.videoId != undefined) {
+                    const link = `https://www.youtube.com/watch?v=${item.id.videoId}`
+                    console.log(link);
 
-                console.log(item.snippet.channelTitle);
+                    // print each link from our query into the results container
+                    resultsContainer.innerHTML += `<h2>${item.snippet.title}</h2>`;
+                    resultsContainer.innerHTML += `<a href="${link}">${link}</a>`;
+                    resultsContainer.innerHTML += `<p>${item.snippet.description}</p>`;
+
+                }
             })
         })
 
