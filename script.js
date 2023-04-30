3f59b61ed0951536b579d80d4563cd09abbbaf94
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-04-29 23:52:15 -0500

    added the code for js to run a query but took out the key

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-const apiKey = "";
+
+
+const searchBtn = document.getElementById('searchBtn');
+
+const googleAPIUrl = "https://www.googleapis.com/youtube/v3/search";
+
+searchBtn.addEventListener("click", (e) => {
+
+    const searchTerm = document.getElementById("searchQuery").value;
+
+    const apiPrefix = "&key=";
+    console.log(searchTerm)
+
+    const searchQuery = "?part=snippet&q=" + searchTerm + apiPrefix + apiKey;  
+    
+    const url = googleAPIUrl + searchQuery
+    
+    fetch(url);
+    
+
+}
+)
